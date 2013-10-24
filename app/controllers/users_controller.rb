b14ba9387224474ef6e1d34d295aee83bcd54004
Ariel Figueroa <figgyscode@me.com>
2013-10-23 20:09:01 -0600

    Sign Up/Sign In 10.23

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,6 +4,13 @@ class UsersController < ApplicationController
     @title = "Sign Up"
   end
 
+# to get our form to render, we must define an @user variable in the controller action corresponding to new.html.erb, 
+# i.e., the new action in the Users controller.
+
+  def new
+    @user = User.new
+  end
+
   # Here I've used params to retrieve the user id. When I make the appropriate 
   # request to the Users controller, params[:id] will be the user id 1, 
   # so the effect is the same as the find method User.find(1)
--- spec/requests/users_pages_spec.rb
+++ spec/requests/users_pages_spec.rb
@@ -1,5 +1,6 @@
 require 'spec_helper'
 
+=begin
 describe "Users pages" do
 
   subject {page}
@@ -19,7 +20,35 @@ describe "Users pages" do
   describe "signup page" do
     before { visit signup_path }
 
-    it { should have_content('Join Up') }
-    it { should have_title(full_title('Join Up')) }
-  end
+    let(:submit) { "Create my account" }
+
+    describe "with invalid information" do
+      it "should not create a user" do
+        expect { click_button submit }.not_to change(User, :count)
+      end
+    end
+
+    describe "with valid information" do
+# The case of valid data is similar, but instead of verifying that the user count doesn’t change, 
+# we check that clicking the button changes the count by 1:
+      before do
+        fill_in "Name",         with: "Example User"
+        fill_in "Email",        with: "user@example.com"
+        fill_in "Password",     with: "foobar"
+        fill_in "Confirmation", with: "foobar"
+      end
+
+      it "should create a user" do
+# When submitting invalid data, we expect the user count not to change; when submitting valid data, 
+# we expect it to change by 1. We can express this in RSpec by combining the expect method with either 
+# the to method or the not_to method. We’ll start with the invalid case since it is simpler; we visit the 
+# signup path and click the button, and we expect it not to change the user count:
+        expect { click_button submit }.to change(User, :count).by(1)
+ # This is for the benefit of the change method, which takes as arguments an object and a symbol and then 
+ # calculates the result of calling that symbol as a method on the object both before and after the block. 
+ # In other words, the above code calculates User.count from Rails Console.  This happens before and affer
+ # the excution of click_button.
+      end
+    end
 end
+=end
