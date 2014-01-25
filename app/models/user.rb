e041d85a731df36daf8f4c3cf15294e24988c3ce
Adam <baca.adam@me.com>
2014-01-24 18:03:58 -0700

    1.24.14 last commit before adding of taggable

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,21 +1,21 @@
 class User < ActiveRecord::Base #ActiveRecord is what Rails use to talk to a Database.
-  # This sets it so the user can only edit their name and email.
-  # Will not allow id, created_at and update_at to be modified in any way.
+# This sets it so the user can only edit their name and email.
+# Will not allow id, created_at and update_at to be modified in any way.
 
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+# Include default devise modules. Others available are:
+# :confirmable, :lockable, :timeoutable and :omniauthable
+devise :database_authenticatable, :registerable,
+       :recoverable, :rememberable, :trackable, :validatable
 
-  #required fields! email is being required someplace else though!
-  validates_presence_of :first_name, :last_name, :phone
-  validates :phone, length: {minimum: 7, maximum: 10}
-  before_save :uppercase_fields
+#required fields! email is being required someplace else though!
+validates_presence_of :first_name, :last_name, :phone
+validates :phone, length: {minimum: 7, maximum: 10}
+before_save :uppercase_fields
 
-  def uppercase_fields
-    self.first_name.capitalize!
-    self.last_name.capitalize!
-  end
-  has_many :tasks
-  has_many :notifications
+def uppercase_fields
+  self.first_name.capitalize!
+  self.last_name.capitalize!
+end
+has_many :tasks
+has_many :notifications
 end
